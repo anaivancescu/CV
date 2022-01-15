8d748b359f86436a5b9258acb6029da64142fe8e
ana.ivancescu <ana.ivancescu@tremend.com>
2022-01-15 16:16:58 +0200

    Updates

---
+++ precache-manifest.22d075f8876c5534c6dfd84421dc0896.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "747016228485858a0278e16337463d49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "298c6a4b2dc0160677bd",
+    "url": "/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "5853fadb91b176fc46b0",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "298c6a4b2dc0160677bd",
+    "url": "/static/js/2.5582ea58.chunk.js"
+  },
+  {
+    "revision": "4cf5c6c6f464cc15458a86cbdc669a15",
+    "url": "/static/js/2.5582ea58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5853fadb91b176fc46b0",
+    "url": "/static/js/main.d90f17cb.chunk.js"
+  },
+  {
+    "revision": "fd5241efd20f9c7180ac",
+    "url": "/static/js/runtime-main.12dae14b.js"
+  },
+  {
+    "revision": "af4daf8720daf5a51aec46c322449ff9",
+    "url": "/static/media/cv.af4daf87.pdf"
+  }
+]);
